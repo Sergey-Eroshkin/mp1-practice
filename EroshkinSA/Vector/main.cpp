9a1789d3e569813fcbdb546c29260e0e645a37fd
Ерошкин Сергей Александрович <s25380312@unn.ru>
2026-03-11 15:35:42 +0300

    Start of Vectors

---
+++ EroshkinSA/Vector/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct Vector {
+	double* x;
+	int n;
+	Vector(int);
+	Vector(const Vector&);
+	~Vector();
+	double len() const;
+	double operator *(const Vector&);
+	Vector operator +(const Vector&);
+	Vector operator -(const Vector&);
+};
+
+Vector::Vector(int n) {
+	this->n = n;
+	this->x = new double[n];
+}
+
+Vector::Vector(const Vector& v) {
+	this->n = v.n;
+	this->x = new double[v.n];
+	for (int i = 0; i < n; i++) this->x[i] = v.x[i];
+}
+
+Vector::~Vector() {
+	delete[] this->x;
+}
+
+double Vector::len() const {
+	double s = 0.0;
+	for (int i = 0; i < n; i++) s += x[i] * x[i];
+	return sqrt(s);
+}
+
+double Vector::operator *(const Vector& v) {
+	if (n != v.n) throw "Enter";
+	double s = 0.0;
+	for (int i = 0; i < n; i++) s += x[i] * v.x[i];
+	return s;
+}
+
+Vector Vector::operator +(const Vector& v) {
+	if (n != v.n) throw "Error";
+	Vector v1(n);
+	for (int i = 0; i < n; i++) v1.x[i] = x[i] + v.x[i];
+	return v1;
+}
+
+Vector Vector::operator -(const Vector& v) {
+	if (n != v.n) throw "Error";
+	Vector v1(n);
+	for (int i = 0; i < n; i++) v1.x[i] = x[i] - v.x[i];
+	return v1;
+}
+
+
+int main() {
+	int n; cin >> n;
+	Vector v(n), v1(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v.x[i];
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> v1.x[i];
+	}
+	cout << v * v1 << endl;
+	Vector v2(v + v1);
+	for (int i = 0; i < n; i++) {
+		cout << v2.x[i] << " ";
+	}
+}
